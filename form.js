ab6cf1658235837192331f1eaf7ae82250b6dac9
Rotationaries <rotationaries@gmail.com>
2023-09-20 00:50:09 -0700

    printing out review values (unfinished)

--- form.js
+++ form.js
@@ -1,19 +1,13 @@
 function storeInput() {
-    const input = [];
+    const inputAry = [];
     
-    const name = document.getElementById("name");
-    const uid = document.getElementById("uid");
-    const pname = document.getElementById("pname");
-    const pid = document.getElementById("pid");
-    const pers = document.getElementById("pers");
-    const rate = document.getElementById("rate");
+    const name = document.getElementById("name").value;
+    const uid = document.getElementById("uid").value;
+    const pname = document.getElementById("pname").value;
+    const pid = document.getElementById("pid").value;
+    const pers = document.getElementById("pers").value;
+    const rate = document.getElementById("rate").value;
 
-    const val1 = name.value;
-    const val2 = uid.value;
-    const val3 = pname.value;
-    const val4 = pid.value;
-    const val5 = pers.value;
-    const val6 = rate.value;
-    input.push(val1, val2, val3,val4,val5,val6);
-    console.log(input);
+    inputAry.push(name, uid, pname, pid, pers, rate);
+    document.write(inputAry.toString());
   }
